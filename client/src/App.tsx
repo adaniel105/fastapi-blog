7452393db0c0a1951a033e449a448b51d614ce11
Daniel Akinola <adaniel@gmail.com>
2024-12-05 23:33:56 +0100

    Styling reset

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button"
 
-export default function App() {
-  return (
-    <div>
-      <Button>Click me</Button>
+export default function App(){
+  return(
+    <div className="mt-20 max-w-3xl font-sans text-lg">
+      <h1>All Blogs</h1>
+
     </div>
   )
 }
