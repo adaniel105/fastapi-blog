30d0a1469128d691093a9be10532c887664b86c3
adaniel105 <dakinola18@gmail.com>
2024-12-06 00:53:28 +0100

    Add sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 
 export default function App(){
   return(
-    <div className="mt-20 max-w-3xl font-sans text-lg">
-      <h1>All Blogs</h1>
+    <div className="mt-20 ml-20 max-w-3xl">
+      <h1 className="text-4xl">All Blogs</h1>
+      <ul className="mt-2 ml-10 list-disc">
+        <li>Blog 1</li>
+        <li>Blog 2</li>
+        <li>Blog 3</li>
+        <li>Blog 4</li>
+        <li>Blog 5</li>
+      </ul>
 
     </div>
   )
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <SidebarProvider>
+      <AppSidebar />
+        <SidebarTrigger />
+          <App />
+    </SidebarProvider>
+  </StrictMode>
 )
