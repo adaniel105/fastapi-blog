10ce77fc153548494e02b90d297d875049e8709f
Daniel Akinola <adaniel@gmail.com>
2024-12-05 23:08:58 +0100

    shadcn init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
-import './App.css'
+import { Button } from "@/components/ui/button"
 
 export default function App() {
   return (
-    
+    <div>
+      <Button>Click me</Button>
+    </div>
   )
 }
