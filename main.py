58fd61b05fad079da684ab0dcb54300e427fc04d
adaniel105 <dakinola18@gmail.com>
2024-12-06 23:32:45 +0100

    configure router

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from core import blog, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
-    title="SecureAPI",
+    title="BlogAPI",
     description="My blog",
     version="1.0.0",)
 
@@ -16,18 +16,3 @@ app.include_router(blog.router)
 app.include_router(auth.router)
 app.include_router(user.router)
 
-
-@app.get("/", response_class=HTMLResponse)
-def index():
-    return """
-<!Doctype html>
-    <html>
-        <body>
-            <h1>SecureAPI</h1>
-            <div class="btn-group">
-                <a href="/docs"><button>SwaggerUI</button></a>
-                <a href="/redoc"><button>Redoc</button></a>
-            </div>
-        </body>
-    </html>
-"""
