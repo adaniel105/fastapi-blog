e9aae7415102cfd87f1cdabb6befefd467391af0
adaniel105 <dakinola18@gmail.com>
2024-12-07 22:57:15 +0100

    Update endpoints and routes

--- core/blog.py
+++ core/blog.py
@@ -1,37 +1,113 @@
-from schema.oa2 import get_current_user
-from sqlalchemy.orm import Session
-from database import configuration
-from fastapi import APIRouter, Depends, status, Response
-from schema import schemas
-from database import configuration
 from typing import List
-from api import blog
+from fastapi import APIRouter, Depends, Response, status
+from sqlalchemy.orm import Session
 
-router = APIRouter(tags=["Blogs"], prefix="/blogs")
+from api import blog
+from database import configuration
+from schema import schemas
+from schema.oa2 import get_current_user
+
+router = APIRouter(tags=["Blogs"], prefix="/blog")
 get_db = configuration.get_db
 
 
-# all actions to be performed from current user, auth purposes
 @router.get("/", response_model=List[schemas.ShowBlog])
-def get_all_blogs(db: Session = Depends(get_db),  current_user: schemas.User = Depends(get_current_user)):
+def get_all_blogs(
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
+    """
+    Get all blogs
+
+    Args:
+        db (Session, optional): Database session. Defaults to None.
+        current_user (schemas.User, optional): Current user. Defaults to None.
+
+    Returns:
+        List[schemas.ShowBlog]: List of blogs
+    """
     return blog.get_all(db)
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def create_blog(request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
+def create(
+    request: schemas.Blog,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
+    """
+    Create a blog
+
+    Args:
+        request (schemas.Blog): Blog to create
+        db (Session, optional): Database session. Defaults to None.
+        current_user (schemas.User, optional): Current user. Defaults to None.
+
+    Returns:
+        schemas.Blog: Created blog
+    """
     return blog.create(request, db)
 
 
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def get_blog_by_id(id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
+def get_blog_by_id(
+    id: int,
+    response: Response,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
+    """
+    Get a blog by id
+
+    Args:
+        id (int): Blog id
+        response (Response): FastAPI response
+        db (Session, optional): Database session. Defaults to None.
+        current_user (schemas.User, optional): Current user. Defaults to None.
+
+    Returns:
+        schemas.ShowBlog: Blog
+    """
     return blog.show(id, db)
 
 
-@router.put("/{id}", status_code=status.HTTP_201_CREATED)
-def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
+    """
+    Delete a blog by id
+
+    Args:
+        id (int): Blog id
+        db (Session, optional): Database session. Defaults to None.
+        current_user (schemas.User, optional): Current user. Defaults to None.
+
+    Returns:
+        None: None
+    """
+    return blog.destroy(id, db)
+
+
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update_blog(
+    id: int,
+    request: schemas.Blog,
+    db: Session = Depends(get_db),
+    current_user: schemas.User = Depends(get_current_user),
+):
+    """
+    Update a blog by id
+
+    Args:
+        id (int): Blog id
+        request (schemas.Blog): Blog to update
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+        current_user (schemas.User, optional): Current user. Defaults to Depends(get_current_user).
+
+    Returns:
+        schemas.Blog: Updated blog
+    """
     return blog.update(id, request, db)
-
-
-@router.post("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_blog(id: int, db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    return blog.delete(id, db)
--- core/user.py
+++ core/user.py
@@ -1,28 +1,57 @@
-from fastapi import APIRouter, Depends, Response, status
-from sqlalchemy.orm import Session
-from schema import schemas
-from models import models
-from database import configuration
+#!/usr/bin/python3
+
 from typing import List
+
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
 from api import user
+from database import configuration
+from schema import schemas
 
 router = APIRouter(tags=["Users"], prefix="/users")
 get_db = configuration.get_db
 
 
-# Create user
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    """
+    Create a new user
+
+    Args:
+        request (schemas.User): User to create
+        db (Session, optional): Database session.
+
+    Returns:
+        schemas.ShowUser: User created
+    """
     return user.create(request, db)
 
 
-# get all users
-@router.get("/", status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
-def get_all(db: Session = Depends(get_db)):
+@router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.ShowUser])
+def get_users(db: Session = Depends(get_db)):
+    """
+    Get all users
+
+    Args:
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+
+    Returns:
+        List[schemas.ShowUser]: List of users
+    """
     return user.get_all(db)
 
 
-# get_users_by_id
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
 def get_user_by_id(id: int, db: Session = Depends(get_db)):
-    user = user.show(id, db)
+    """
+    Get a user by id
+
+    Args:
+        id (int): User id
+        db (Session, optional): Database session. Defaults to Depends(get_db).
+
+    Returns:
+        schemas.ShowUser: User
+    """
+    return user.show(id, db)
