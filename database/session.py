09344a342c7870c773508c8b4c1c079a70307fa6
adaniel105 <dakinola18@gmail.com>
2024-12-18 21:48:20 +0100

    postgres db conn

--- database/session.py
+++ database/session.py
@@ -1,12 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-#from core import config
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
