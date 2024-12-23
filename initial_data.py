f6f2932dea92b393941f950d94f9fcf48795df4a
adaniel105 <dakinola18@gmail.com>
2024-12-23 23:40:32 +0100

    Add initial data

---
+++ initial_data.py
@@ -0,0 +1,21 @@
+from api import user
+from schema import schemas
+from database.session import SessionLocal
+
+
+def init() -> None:
+    db = SessionLocal()
+    user.create(
+        request= schemas.User(
+            first_name="John",
+            last_name = "Doe",
+            email = "JohnDoe@gmail.com",
+            password = "qwerty",
+        ),
+        db=db
+    ),
+
+if __name__ == "__main__":
+    print("Creating user John Doe")
+    init()
+    print("User created")
